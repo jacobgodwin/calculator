27efe282d276c4926570dfa94e4d657950de6fff
Jacob Godwin <jpg174@gmail.com>
2022-01-25 11:39:23 -0500

    modify function for operate and num

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ let nextValue = 0;
 let selectedOperator = "add";
 let displayValue = document.getElementById("dispValue");
 let operators = document.getElementsByClassName("operation");
-let total = 0;
+let total = null;
+let decSelected = false;
+
 const numbers = document.getElementsByClassName("number-btn");
 const clearBtn = document.getElementById("clear");
 const equalSign = document.getElementById("equals");
+const decBtn = document.getElementById("decimal");
 
 function add(x, y) {
   let sum = 0;
+  x = Number(x);
+  y = Number(y);
   sum = x + y;
   return Math.round((sum + Number.EPSILON) * 100) / 100;
 }
 
 function subtract(x, y) {
   let diff = 0;
+  x = Number(x);
+  y = Number(y);
   diff = x - y;
   return Math.round((diff + Number.EPSILON) * 100) / 100;
 }
 
 function multiply(x, y) {
   let prod = 0;
+  x = Number(x);
+  y = Number(y);
   prod = x * y;
   return Math.round((prod + Number.EPSILON) * 100) / 100;
 }
@@ -31,6 +40,8 @@ function divide(x, y) {
   if (y === 0) {
     return "ERROR";
   } else {
+    x = Number(x);
+    y = Number(y);
     quot = x / y;
     return Math.round((quot + Number.EPSILON) * 100) / 100;
   }
@@ -48,11 +59,23 @@ function operate(selectedOperator, x, y) {
   }
 }
 
-equalSign.addEventListener("click", totalUp);
-
 function totalUp() {
+  operate(selectedOperator, initialValue, nextValue);
+  console.log("total: " + total);
+  console.log("IV: " + initialValue);
+  console.log("NV: " + nextValue);
+  console.log("Op: " + selectedOperator);
   displayValue.innerHTML = total;
   removeClass();
+  decSelected = false;
+}
+
+function addDecimal() {
+  if (!decSelected) {
+    decSelected = true;
+    displayValue.innerHTML += ".";
+    nextValue += ".";
+  }
 }
 
 function clearDisplay() {
@@ -61,6 +84,7 @@ function clearDisplay() {
   nextValue = 0;
   total = null;
   selectedOperator = "add";
+  decSelected = false;
   removeClass();
   console.log("total: " + total);
   console.log("IV: " + initialValue);
@@ -69,19 +93,16 @@ function clearDisplay() {
 }
 
 function numDisplay(e) {
-  if (total === null) {
-    total = 0;
-    displayValue.innerHTML = parseInt(e.target.textContent);
-    nextValue = parseInt(e.target.textContent);
-    operate(selectedOperator, initialValue, nextValue);
+  if (decSelected) {
+    nextValue += parseFloat(e.target.textContent);
+    displayValue.innerHTML += parseFloat(e.target.textContent);
     console.log("total: " + total);
     console.log("IV: " + initialValue);
     console.log("NV: " + nextValue);
     console.log("Op: " + selectedOperator);
   } else {
-    displayValue.innerHTML = parseInt(e.target.textContent);
-    nextValue = parseInt(e.target.textContent);
-    operate(selectedOperator, initialValue, nextValue);
+    nextValue = parseFloat(e.target.textContent);
+    displayValue.innerHTML = parseFloat(e.target.textContent);
     console.log("total: " + total);
     console.log("IV: " + initialValue);
     console.log("NV: " + nextValue);
@@ -89,41 +110,58 @@ function numDisplay(e) {
   }
 }
 
-clearBtn.addEventListener("click", clearDisplay);
-
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", numDisplay);
-  numbers[i].addEventListener("click", operate);
-}
-
-for (let i = 0; i < operators.length; i++) {
-  operators[i].addEventListener("click", addClass);
+function selectOperator(e) {
+  if (total === null && !decSelected && initialValue === 0) {
+    operate(selectedOperator, initialValue, nextValue);
+    removeClass();
+    addClass(e);
+    selectedOperator = e.target.id;
+    initialValue = displayValue.innerHTML;
+    total = displayValue.innerHTML;
+    console.log("total: " + total);
+    console.log("IV: " + initialValue);
+    console.log("NV: " + nextValue);
+    console.log("Op: " + selectedOperator);
+  } else if (total === null && decSelected && initialValue === 0) {
+    decSelected = false;
+    removeClass();
+    addClass(e);
+    selectedOperator = e.target.id;
+    initialValue = displayValue.innerHTML;
+    total = displayValue.innerHTML;
+    console.log("total: " + total);
+    console.log("IV: " + initialValue);
+    console.log("NV: " + nextValue);
+    console.log("Op: " + selectedOperator);
+  } else if (decSelected) {
+    operate(selectedOperator, initialValue, nextValue);
+    decSelected = false;
+    removeClass();
+    addClass(e);
+    selectedOperator = e.target.id;
+    initialValue = total;
+    displayValue.innerHTML = total;
+    console.log("total: " + total);
+    console.log("IV: " + initialValue);
+    console.log("NV: " + nextValue);
+    console.log("Op: " + selectedOperator);
+  } else {
+    operate(selectedOperator, initialValue, nextValue);
+    removeClass();
+    addClass(e);
+    selectedOperator = e.target.id;
+    initialValue = total;
+    displayValue.innerHTML = total;
+    console.log("total: " + total);
+    console.log("IV: " + initialValue);
+    console.log("NV: " + nextValue);
+    console.log("Op: " + selectedOperator);
+  }
 }
 
 function addClass(e) {
-  if (total === null) {
-    removeClass();
-    e.target.classList.remove("operation");
-    e.target.classList.add("operation-selected");
-    selectedOperator = e.target.id;
-    initialValue = parseInt(displayValue.innerHTML);
-    nextValue = 0;
-    console.log("total: " + total);
-    console.log("IV: " + initialValue);
-    console.log("NV: " + nextValue);
-    console.log("Op: " + selectedOperator);
-  } else {
-    removeClass();
-    e.target.classList.remove("operation");
-    e.target.classList.add("operation-selected");
-    selectedOperator = e.target.id;
-    initialValue = total;
-    nextValue = 0;
-    console.log("total: " + total);
-    console.log("IV: " + initialValue);
-    console.log("NV: " + nextValue);
-    console.log("Op: " + selectedOperator);
-  }
+  e.target.classList.remove("operation");
+  e.target.classList.add("operation-selected");
 }
 
 function removeClass() {
@@ -134,4 +172,18 @@ function removeClass() {
       clicked.className = "operation";
     }
   }
+}
+
+decBtn.addEventListener("click", addDecimal);
+
+equalSign.addEventListener("click", totalUp);
+
+clearBtn.addEventListener("click", clearDisplay);
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", numDisplay);
+}
+
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", selectOperator);
 }
